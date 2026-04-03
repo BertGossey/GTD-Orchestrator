9f7e065324176c6d199ed7570d45204ae43c50cc
bert.gossey <bgsy@foreach.be>
2026-04-03 14:04:16 +0200

    feat: update someday page to use TaskListWithProvider

--- src/app/(gtd)/someday/page.tsx
+++ src/app/(gtd)/someday/page.tsx
@@ -1,6 +1,6 @@
 import { getTasksBySection } from "@/actions/tasks";
 import { getActiveProjects } from "@/actions/projects";
-import { TaskList } from "@/components/gtd/task-list";
+import { TaskListWithProvider } from "@/components/gtd/task-list-with-provider";
 
 export default async function SomedayPage() {
   const [tasks, projects] = await Promise.all([
@@ -11,7 +11,7 @@ export default async function SomedayPage() {
   return (
     <div>
       <h1 className="mb-4 text-xl font-semibold">Someday</h1>
-      <TaskList
+      <TaskListWithProvider
         tasks={tasks}
         projects={projects.map((p) => ({ id: p.id, title: p.title }))}
         sectionId="SOMEDAY"
